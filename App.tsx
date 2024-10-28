4ca27bc83ff7354cb566f32a33e8559f9a824d8f
geisabitt <ageisislane@gmail.com>
2024-10-27 22:13:53 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View } from "react-native";
+import { Product } from "./components/products";
+
+const products = [
+  {
+    id: "1",
+    title: "Inteligência Emocional",
+    price: 40.99,
+    image: require("./assets/mock/1.png"), // Usando require() para imagem local
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "2",
+    title: "Deixe de ser Pobre",
+    price: 40.99,
+    image: require("./assets/mock/2.png"),
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "3",
+    title: "Desenvolvendo Aplicativos com Chat GPT",
+    price: 40.99,
+    image: require("./assets/mock/3.png"),
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "4",
+    title: "Código Limpo",
+    price: 40.99,
+    image: require("./assets/mock/4.png"),
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "5",
+    title: "Gatilhos Mentais",
+    price: 40.99,
+    image: require("./assets/mock/5.png"),
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "6",
+    title: "Inteligência Emocional",
+    price: 40.99,
+    image: require("./assets/mock/1.png"), // Usando require() para imagem local
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "7",
+    title: "Deixe de ser Pobre",
+    price: 40.99,
+    image: require("./assets/mock/2.png"),
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "8",
+    title: "Desenvolvendo Aplicativos com Chat GPT",
+    price: 40.99,
+    image: require("./assets/mock/3.png"),
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "9",
+    title: "Código Limpo",
+    price: 40.99,
+    image: require("./assets/mock/4.png"),
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "10",
+    title: "Gatilhos Mentais",
+    price: 40.99,
+    image: require("./assets/mock/5.png"),
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList data={products} keyExtractor={(item) => item.id} renderItem={({ item }) => <Product id={item.id} title={item.title} price={item.price} image={item.image} description={item.description} />} numColumns={2} contentContainerStyle={styles.listContainer} />
     </View>
   );
 }
@@ -13,8 +85,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#141518",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
+  },
+  listContainer: {
+    minWidth: "100%",
+    maxWidth: 412,
+    marginVertical: 0,
+    marginHorizontal: "auto",
+    padding: 0,
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 20,
   },
 });
---
+++ components/products.tsx
@@ -0,0 +1,69 @@
+import { StyleSheet, Image, Text, View } from "react-native";
+
+export interface ProductProps {
+  id: string;
+  title: string;
+  price: number;
+  image: any;
+  description?: string;
+}
+
+export function Product(props: ProductProps) {
+  return (
+    <View id={props.id} style={styles.container}>
+      <Image style={styles.img} source={props.image} />
+      <View style={styles.containerText}>
+        <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">
+          {props.title}
+        </Text>
+        <Text style={styles.description} numberOfLines={2} ellipsizeMode="tail">
+          {props.description}
+        </Text>
+        <Text style={styles.price}>R$ {props.price}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    maxWidth: 192,
+    maxHeight: 280,
+    marginVertical: 10,
+    marginHorizontal: 6,
+    borderRadius: 10,
+    backgroundColor: "#4B4B4B",
+  },
+  containerText: {
+    padding: 10,
+  },
+  img: {
+    maxWidth: "100%",
+    maxHeight: 190,
+    resizeMode: "cover",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  title: {
+    color: "#ffffff",
+    fontFamily: "Roboto",
+    fontSize: 14,
+    fontStyle: "normal",
+    fontWeight: "400",
+  },
+  description: {
+    color: "#ffffff",
+    fontFamily: "Roboto",
+    fontSize: 10,
+    fontStyle: "normal",
+    fontWeight: "400",
+  },
+  price: {
+    color: "#448B48",
+    fontFamily: "Roboto",
+    fontSize: 12,
+    fontStyle: "normal",
+    fontWeight: "400",
+  },
+});
